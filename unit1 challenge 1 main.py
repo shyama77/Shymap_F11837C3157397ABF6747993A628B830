f9f61e824c6e15604ac1463695b7ff9af33f8688
shyama77 <145428892+shyama77@users.noreply.github.com>
2023-09-22 14:50:31 +0530

    Update unit1 challenge 11 main.py

--- unit1 challenge 1 main.py
+++ unit1 challenge 1 main.py
@@ -1,5 +1,8 @@
-def factorial(n):
-  if n == 0:
+def fact(n):
+  if n<=1:
     return 1
   else:
-    return n * factorial(n - 1)
+    n=n*fact(n-1)
+    return n
+n=int(input("enter number:"))
+print("factorial of ",n,"is",fact (n))
